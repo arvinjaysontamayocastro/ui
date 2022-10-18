1582011b51e49fd28ef564b031dd23c3802de8c9
AO <arvinjaysontamayocastro@gmail.com>
2022-10-18 15:52:12 +0800

    Initial Commit
    Initial Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth-guard.service';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoginComponent } from './login/login.component';
+import { ProductsComponent } from './products/products.component';
+
+//all components routes
+const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'product', component: ProductsComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
